d87c3c76adaf8664ef0591ffb93cbbfca473d374
Jacob Rosen <jacobrosen819@gmail.com>
2021-06-23 01:18:49 +0000

    started to design the game aspect

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+"""
+Using random to randomamly assign scores to questions as well as
+random give questions
+"""
+import random
+"""
+importing sqlite 3 in order to keep scores in a database
+"""
+import sqlite3
+#class for game
+class quiz:
+  def __init__ (self, bank, questions, list_of_questions)
+def quiz(bank):
+  question_1 = input
+
 #Title welocme to the game
 print("Welcome to the car quiz!!!\n ")
 #asking if they want to play
@@ -5,5 +20,10 @@ playing = input("Do you want to play? ").lower()
 #
 if playing == "yes" or playing == "y" or playing == "yea" or playing == "yeah":
   print("The goal of the game is to get over 500 points. \nQuestions are worth between 10 and 75 points. \nThier values are randomly assigned\n \nLETS GO")
+  bank = 0
+  while bank <= 500:
+    quiz(bank)
+    #bank = int(input("BANK: "))
+
 else:
   quit()
